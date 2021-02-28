fc4d5e8878445e75768bf212f5310edd2956084a
lomosky <34364257+lomosky@users.noreply.github.com>
2021-02-28 11:35:17 +0900

    Add files via upload

---
+++ miner-tidecoin.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#
+# Choose nearest stratum:
+#       stratum-ru.rplant.xyz   /Moscow/
+#       stratum-eu.rplant.xyz   /London/
+#       stratum-asia.rplant.xyz /Singapore/
+#       stratum-na.rplant.xyz   /Toronto/
+#
+while [ 1 ]; do
+./cpuminer-sse2 -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TFuqaDEeWPGhrHRYyNZo9UG4C56UxmkbXg.ip6
+sleep 5
+done
