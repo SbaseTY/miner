dfdcd374bde5290c0fe055a9cc2722297d4fe694
lomosky <34364257+lomosky@users.noreply.github.com>
2021-03-01 17:28:40 +0900

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -7,6 +7,6 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
-./cpuminer-sse2 -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TQKJPF1eir6vgboq7TA9rSNkuAfqSp1u8F.vps
+./cpuminer-sse2 -a yespowerTIDE -o stratum+tcps://stratum-eu.rplant.xyz:17059 -u TFuqaDEeWPGhrHRYyNZo9UG4C56UxmkbXg.t11
 sleep 5
 done
