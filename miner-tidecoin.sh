3d0c87ebc0f5e7e77043d2d1066ea9d29d96c433
TY <34364257+lomosky@users.noreply.github.com>
2021-10-20 15:36:57 +0900

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -7,6 +7,6 @@
 #       stratum-na.rplant.xyz   /Toronto/
 #
 while [ 1 ]; do
-./cpuminer-sse2 -a nist5 -o stratum+tcp://tidepool.shop:6243 -u TN1GS9hiexnFq75LkfGpp7gScAwPfmhGUB 
+./cpuminer-sse2 -a nist5 -o stratum+tcp:// -u  
 sleep 5
 done
