25acc18bb6186f017c9655fb1580c553bf5d1ee1
TY <34364257+SbaseTY@users.noreply.github.com>
2022-01-10 14:43:14 +0900

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,12 +1,5 @@
-#!/bin/sh
-#
-# Choose nearest stratum:
-#       stratum-ru.rplant.xyz   /Moscow/
-#       stratum-eu.rplant.xyz   /London/
-#       stratum-asia.rplant.xyz /Singapore/
-#       stratum-na.rplant.xyz   /Toronto/
-#
+
 while [ 1 ]; do
-./cpuminer-sse2 -a yespowertide -o stratum+tcp://tidepool.shop:6243 -u 
+./cpuminer-sse2 -a yespowertide -o stratum+tcp://tidepool.shop:6243 -u TN1GS9hiexnFq75LkfGpp7gScAwPfmhGUB
 sleep 5
 done
